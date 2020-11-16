9e73e9b666d270344b9a88f1b69ec4eeb99b74d0
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-16 13:24:02 -0500

    commit tree e56eeb367697a32fd5fe4bf63a4a1f548d9f1c74

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 14, "M", "mixed", "happy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "F", "brown", "sad", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 7, "F", "white", "happy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10,  "M", "black", "stern", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "F", "Pink", "happy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 1, "M", "white", "neutral", 0);
+INSERT INTO bears VALUES (7, "Wendy", 2, "F", "brown", "goofy", 0);
+INSERT INTO bears VALUES (8, NULL, 3, "M", "black", "angry", 1);
